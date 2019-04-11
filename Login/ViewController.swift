0ff7cca65b023d0909a49ed8578c38296d3e2971
Sim Yong Seng <simyseng@gmail.com>
2019-04-11 22:00:50 +0700

    Step 2

--- Login/ViewController.swift
+++ Login/ViewController.swift
@@ -10,11 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        segue.destination.navigationItem.title = usernameTextField.text
+    }
 }
 
