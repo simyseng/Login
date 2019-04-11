1464a199584bdc5cb5b8a977355479f4d1f441c0
Sim Yong Seng <simyseng@gmail.com>
2019-04-11 21:10:58 +0700

    Initialize commit

---
+++ Login/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Login
+//
+//  Created by Sim Yong Seng on 11/4/19.
+//  Copyright © 2019 Sim Yong Seng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
